58b550ebb8403ce332a56f2a79b9c04410ec0d54
LekoArts <lekoarts@gmail.com>
2020-04-02 15:49:53 +0200

    Add 85901 error

--- sites/85901/src/pages/index.js
+++ sites/85901/src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({ data }) => <div>{JSON.stringify(data, null, 2)}</div>
+
+export const query = graphql`
+  {
+    site {
+      buildTime(length: 10)
+    }
+  }
+`
